3cd70e646368e0839c3df637f379c6d158d7c523
IshaanD-RX6600 <dhimanishaan7@gmail.com>
2026-03-20 18:00:33 -0400

    fix: adjust chatbot component positioning for improved layout

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -58,7 +58,7 @@ export default function Chatbot() {
   };
 
   return (
-    <div className="fixed bottom-6 left-6 z-50 flex flex-col items-start pointer-events-none">
+    <div className="fixed bottom-24 sm:bottom-6 right-6 z-50 flex flex-col items-end pointer-events-none">
       <AnimatePresence>
         {isOpen && (
           <motion.div
